689e0d79fc4b8aae6937b1c166bf197156bdc4da
samsour <sam.sauer@hotmail.de>
2018-06-27 14:44:00 +0200

    remove console logs

--- js/main.js
+++ js/main.js
@@ -1,10 +1,7 @@
 console.log("Loading...");
 
 $(document).ready(function () {
-	console.log("Starting...");
-
 	$('.splitscreen').fullpage({ verticalCentered: false });
 	$('.loader').fadeOut('slow');
 	$('.splitscreen').fadeIn('slow');
-	console.log("Finished!");
 });
