fecea529c5fabcb6be3fcd5dfd50e0158853f362
samsour <sam.sauer@hotmail.de>
2018-06-27 18:43:01 +0200

    corrupted: add build task

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,11 +1,16 @@
-console.log("Loading...");
+import $ from 'jquery';
+import fullpage from 'fullpage.js';
 
 $(document).ready(function () {
+
 	$('.splitscreen').fullpage({ verticalCentered: false });
+
 	setTimeout( () => {
 		$('.loader').addClass('hidden');
 	}, 300);
+
 	setTimeout( () => {
 		$('.splitscreen').removeClass('hidden');
 	}, 600);
+
 });
